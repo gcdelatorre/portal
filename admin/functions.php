d6f54bd6134acd397d6b611fbd26722f70e20d35
tenzies-game <giancarlo.delatorre.dev@gmail.com>
2025-11-10 23:56:23 +0800

    finished adding generate random password and username feature

--- admin/functions.php
+++ admin/functions.php
@@ -3,6 +3,11 @@
 
 function addToScholars($name, $email, $conn) {
     // Basic SQL concatenation - VULNERABLE TO SQL INJECTION
+
+    $username = generateUsername($name);
+    $password = generateRandomPassword(7);
+    addtoScholars($username, $password, $conn);
+
     $sql = "INSERT INTO `scholars` (`name`, `email`, `date`) 
             VALUES ('$name', '$email' , current_timestamp())";
 
@@ -14,6 +19,51 @@ function addToScholars($name, $email, $conn) {
         $_SESSION['message'] = "<p style='color:red;'>Could not add scholar: " . $e->getMessage() . "</p>";
     }
 }
+
+function addScholarAccount($scholar_id, $username, $password) {
+    global $conn;
+
+    $sql = "INSERT INTO `accounts` (`scholar_id`, `username`, `password`)
+            VALUES ('$scholar_id', '$username', '$password')";
+
+    try {
+        mysqli_query($conn, $sql);
+    }
+    catch (mysqli_sql_exception) {
+        // Handle error (e.g., log it)
+        echo "Error adding scholar account.";   
+    }
+}
+
+
+function generateRandomPassword ($length = 8) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $charactersLength = strlen($characters);
+    $randomPassword = '';
+    for ($i = 0; $i < $length; $i++) {
+        $randomPassword .= $characters[rand(0, $charactersLength - 1)];
+    }
+    return $randomPassword;
+}
+
+    echo generateRandomPassword(6);
+
+function generateUsername ($name) {
+    $nameParts = explode(' ', strtolower($name));
+    $username = '';
+
+    if (count($nameParts) >= 2) {
+        $username = $nameParts[0][0] . $nameParts[1]; // first initial + last name
+    } else {
+        $username = $nameParts[0]; // single name
+    }
+
+    // Append random number to ensure uniqueness
+    $username .= rand(100, 999);
+
+    return $username;
+}
+
 
 function removeApplication($applicant_id, $conn) {
     // Basic SQL concatenation - VULNERABLE TO SQL INJECTION
